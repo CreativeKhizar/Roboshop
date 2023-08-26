1c4cfafdcf3c785292f37d68c11cb356864914d0
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-26 15:05:58 +0530

    Files added

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,31 @@
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shell-script-logs/script-name-date.log
 
+SCRIPT_NAME=$0
+
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$whoami
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+if [ $USERID != "root" ]; then
+echo "$R ERROR:: Please run this script with root access $N"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 VALIDATE(){
     if [ $1 -ne 0 ]
